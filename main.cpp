34e2d12c0f460fedfcb71fa1b339f5bb545f0066
Derek-uwi <Derek-uwi@users.noreply.github.com>
2016-09-30 17:21:08 -0400

    added comments

--- main.cpp
+++ main.cpp
@@ -6,35 +6,35 @@ using namespace std;
 int main()
 {
   vector<string> myreplstr;//initialize empty vector
-  string s, ch, replstr;
-  int r, n, i;
+  string s, ch, replstr;//initialize strings
+  int r, n, i;//initialize integers
   
   //input
-  cout<< "Please enter a string with no spaces." << endl;
-  getline(cin, s);
+  cout<< "Please enter a string with no spaces." << endl;//prompt user to enter string with no spaces
+  getline(cin, s);//input of string
     while (s.find (' ') != string::npos)//validation that string has no spaces
       {
-        cout << "String cannot have any spaces!";
-        cout<< "Please enter a string with no spaces." << endl;
-        getline(cin, s);
+        cout << "String cannot have any spaces!";//if string has spaces this message will be printed
+        cout<< "Please enter a string with no spaces." << endl;//prompt user to enter string with no spaces again
+        getline(cin, s);//input of string
       }
-  cout << "Please enter an integer." << endl;
-  cin >> r;
-   if (r == 1)
+  cout << "Please enter an integer." << endl;//prompt user to enter an integer
+  cin >> r;//input integer
+   if (r == 1)//if r is equal to 1 print the following statement
         {
             cout << "Please enter " << r << " pair of characters and strings" << endl;
         }
-        else if (r > 1)
+        else if (r > 1)//if r is more than 1 print the following statement
         {
             cout << "Please enter " << r << " pairs of characters and strings" << endl;
         }
   //figure out how to use vector for character and strings
-  for (i = 0; i < r; i++ )
+  for (i = 0; i < r; i++ )//while i is less than r it will allow input of ch and replstr
   {
      cin >> replstr;
-    myreplstr.push_back(replstr);
+    myreplstr.push_back(replstr);//adds elements to the vector myreplstr
   }
-   cout << "Please enter an integer." << endl;
+   cout << "Please enter an integer." << endl;//prompt user to enter an integer
   cin >> n;
   
   //output
