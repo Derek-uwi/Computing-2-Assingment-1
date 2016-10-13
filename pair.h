c2a6ef54b7675e48c273187de469dc3abbbe2f13
Derek-uwi <Derek-uwi@users.noreply.github.com>
2016-10-13 19:26:29 -0400

    Create pair.h

---
+++ pair.h
@@ -0,0 +1,13 @@
+#ifndef PAIR_H //header guard
+#define PAIR_H //header guard
+#include <string>
+
+Class Pair
+{
+  Private:
+    char ch;
+    string replstr;
+  Public:
+    Pair();//constructor
+}
+#endif
