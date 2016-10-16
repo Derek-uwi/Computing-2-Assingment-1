b9366f78de2999f7384aba308f73988ddfaeddc5
Derek-uwi <Derek-uwi@users.noreply.github.com>
2016-10-15 20:27:25 -0400

    Update pair.h

--- pair.h
+++ pair.h
@@ -7,7 +7,17 @@ Class Pair
   Private:
     char ch;
     string replstr;
+    vector <string> myreplstr;
+    vector <char> mych;
+    
   Public:
     Pair();//constructor
-};
+    void getch(char);
+    void getreplstr(string);
+    
+    char setch(char);
+    string setreplstr(string);
+    ~Pair()    
+}
 #endif
+
