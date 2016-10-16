e5cd562febf12884fba0ba6633dc1672698c7ab9
Derek-uwi <Derek-uwi@users.noreply.github.com>
2016-10-15 22:18:55 -0400

    Update pair.cpp

--- pair.cpp
+++ pair.cpp
@@ -1,6 +1,31 @@
 #include "Pair.h"
 
-Pair::Pair(char ch, string replstr)
+Pair::Pair()
 {
-  
+  ch =  " ";
+  replstr = " ";
+}
+
+void Pair::setch(char a)
+{
+  ch.push_back(a)
+}
+
+void Pair::setreplstr(string s)
+{
+  replstr.push_back(s)
+}
+
+string Pair::getreplstr(int g)
+{
+  return replstr[g];
+}
+
+char Pair::getch(int d)
+{
+  return ch[d];
+}
+
+Pair::~Pair()
+{
 }
